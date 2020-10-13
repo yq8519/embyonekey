933f8b7759b79de7aefb88d9f6525d10321a8480
yq8519 <yq8518@hotmail.com>
2020-10-13 13:09:24 +0800

    Update embyonekey.sh

--- embyonekey.sh
+++ embyonekey.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd /var/packages/EmbyServer/target/mono/bin
-curl https://raw.githubusercontent.com/s1oz/embyonekey/master/mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
+curl https://raw.githubusercontent.com/yq8519/embyonekey/master/mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
 ./cert-sync /etc/ssl/certs/ca-certificates.crt
 cd /usr/local/etc/nginx/conf.d/*-*-*-*
-wget https://raw.githubusercontent.com/s1oz/embyonekey/master/user.conf
+wget https://raw.githubusercontent.com/yq8519/embyonekey/master/user.conf
 nginx -s reload
 
